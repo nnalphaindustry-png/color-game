c70f00355f7a50b60211e0bdfbdef163f61e4ea2
nnalphaindustry-png <nnalphaindustry@gmail.com>
2026-07-07 22:37:46 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ async function generateMultiGameResult(mode) {
         const winningBigSmall = winningNumber >= 5 ? 'Big' : 'Small';
         
         let winningColor = 'Red';
-        let greenNums = [1, 3, 7, 9];
+        let greenNums =;
         if (greenNums.includes(winningNumber)) winningColor = 'Green';
         if (winningNumber === 5) winningColor = 'Green-Violet';
         if (winningNumber === 0) winningColor = 'Red-Violet';
